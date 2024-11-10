be06785738139d792a36cce6a59d7768d78f6aed
Fraser Molyneux <fmolyneux@microsoft.com>
2024-11-10 19:30:23 +0000

    Index

--- alz-workload.tf
+++ alz-workload.tf
@@ -2,7 +2,7 @@ module "workloads" {
   source = "./modules/alz-workload"
 
   alz_workload_settings               = var.alz_workload_settings
-  private_dns_zones_resource_group_id = module.hub_and_spoke_vnet.private_dns_zones_resource_group_id
+  private_dns_zones_resource_group_id = module.hub_and_spoke_vnet[0].private_dns_zones_resource_group_id
 
   providers = {
     github = github
