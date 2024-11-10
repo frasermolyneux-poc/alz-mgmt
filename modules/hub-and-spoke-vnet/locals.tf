cb481963bdabcd37605fd54fcfe1c9e8e40ca4d4
Fraser Molyneux <fmolyneux@microsoft.com>
2024-11-10 14:31:01 +0000

    hmmm?

--- modules/hub-and-spoke-vnet/locals.tf
+++ modules/hub-and-spoke-vnet/locals.tf
@@ -8,7 +8,7 @@ locals {
     for hub_network_key, hub_network_value in var.hub_virtual_networks : "${hub_network_key}-express-route" => {
       hub_network_key         = hub_network_key
       virtual_network_gateway = hub_network_value.virtual_network_gateways.express_route
-    } if can(hub_network_value.virtual_network_gateways.express_route)
+    } if can(hub_network_value.virtual_network_gateways.express_route != null)
   }
   virtual_network_gateways_vpn = {
     for hub_network_key, hub_network_value in var.hub_virtual_networks : "${hub_network_key}-vpn" => {
