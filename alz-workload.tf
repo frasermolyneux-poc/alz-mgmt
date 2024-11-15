94657f5977e565ea4c40f90617ead079bc8867a6
Fraser Molyneux <fmolyneux@microsoft.com>
2024-11-15 09:57:32 +0000

    Comment workloads

--- alz-workload.tf
+++ alz-workload.tf
@@ -1,10 +1,10 @@
-module "workloads" {
-  source = "./modules/alz-workload"
-
-  alz_workload_settings                 = var.alz_workload_settings
-  private_dns_zones_resource_group_name = var.hub_virtual_networks.hub_and_spoke_vnet_virtual_networks.primary.private_dns_zones.resource_group_name
-
-  providers = {
-    github = github
-  }
-}
+//module "workloads" {
+//  source = "./modules/alz-workload"
+//
+//  alz_workload_settings                 = var.alz_workload_settings
+//  private_dns_zones_resource_group_name = var.hub_virtual_networks.hub_and_spoke_vnet_virtual_networks.primary.private_dns_zones.resource_group_name
+//
+//  providers = {
+//    github = github
+//  }
+//}
