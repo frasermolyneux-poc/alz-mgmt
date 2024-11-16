dd649114ecaa30f5476da6268944729eb339bbcd
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-16 20:09:05 +0000

    hmm

--- modules/hub-and-spoke-vnet/outputs.tf
+++ modules/hub-and-spoke-vnet/outputs.tf
@@ -1,3 +1,3 @@
 output "private_dns_zone_ids" {
-  value = module.private_dns_zones.private_dns_zone_resource_ids
+  value = module.private_dns_zones.combined_private_link_private_dns_zones_replaced_with_vnets_to_link
 }
