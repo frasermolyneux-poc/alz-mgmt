61e6614484e2c556122ccaa3242d6fe1b4f42813
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-16 19:44:14 +0000

    Adding output debugging

---
+++ modules/hub-and-spoke-vnet/outputs.tf
@@ -0,0 +1,3 @@
+output "private_dns_zone_ids" {
+  value = module.private_dns_zones.private_dns_zone_resource_ids
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "hub_and_spoke_vnet_virtual_networks" {
   value = local.hub_and_spoke_vnet_virtual_networks
 }
+
+output "private_dns_zone_ids" {
+  value = module.hub_and_spoke_vnet.private_dns_zone_ids
+}
