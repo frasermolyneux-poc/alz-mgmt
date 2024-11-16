dbfd838019f0eec2762925e22ba5f33c69870097
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-16 18:28:33 +0000

    Update alz workloads variables

--- variables-alz-workloads.tf
+++ variables-alz-workloads.tf
@@ -1,5 +1,12 @@
 variable "alz_workload_settings" {
-  type        = any
+  type = object({
+    location   = string
+    github_org = string
+    virtual_network_settings = object({
+      virtual_network_address_space = string
+      subnets                       = any
+    })
+  })
   default     = {}
   description = <<DESCRIPTION
 The shared settings for the alz workloads. This is where global resources are defined.
