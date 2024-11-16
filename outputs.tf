abd338682a86ca869287f059d5075cbe001bc193
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-16 19:46:45 +0000

    Index

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "hub_and_spoke_vnet_virtual_networks" {
 }
 
 output "private_dns_zone_ids" {
-  value = module.hub_and_spoke_vnet.private_dns_zone_ids
+  value = module.hub_and_spoke_vnet[0].private_dns_zone_ids
 }
