ec92e82daadef90e5c5a505f7d4714ca0407ecd8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-17 17:06:22 +0000

    Outputs

--- modules/hub-and-spoke-vnet/outputs.tf
+++ modules/hub-and-spoke-vnet/outputs.tf
@@ -1,3 +1,7 @@
+output "private_dns_zones_output" {
+  value = module.private_dns_zones
+}
+
 output "private_dns_zone_ids" {
   value = module.private_dns_zones.primary.private_dns_zone_resource_ids
 }
