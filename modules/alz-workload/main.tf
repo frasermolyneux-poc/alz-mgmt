11830d0a06fff2b684861d3f6a5fe5422375e478
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-11-17 19:04:07 +0000

    Add key vault assignment and secret

--- modules/alz-workload/main.tf
+++ modules/alz-workload/main.tf
@@ -99,45 +99,23 @@ module "key_vault" {
   }
 }
 
-//resource "random_id" "kv_id" {
-//  byte_length = 4
-//}
-//
-//data "azurerm_client_config" "current" {}
-//
-//resource "azurerm_key_vault" "github_kv" {
-//  name                = "kv-${random_id.kv_id.hex}-${local.location}"
-//  location            = azurerm_resource_group.rg_github_kv.location
-//  resource_group_name = azurerm_resource_group.rg_github_kv.name
-//  tenant_id           = data.azurerm_client_config.current.tenant_id
-//
-//  soft_delete_retention_days = 90
-//  purge_protection_enabled   = true
-//  enable_rbac_authorization  = true
-//
-//  sku_name = "standard"
-//
-//  network_acls {
-//    bypass         = "AzureServices"
-//    default_action = "Allow"
-//  }
-//}
-//
-//resource "azurerm_role_assignment" "deploy_principal_kv_role_assignment" {
-//  scope                = azurerm_key_vault.github_kv.id
-//  role_definition_name = "Key Vault Secrets Officer"
-//  principal_id         = data.azurerm_client_config.current.object_id
-//}
-//
-//resource "azurerm_key_vault_secret" "github_runners_access_token" {
-//  name         = "github-runners-access-token"
-//  value        = "placeholder"
-//  key_vault_id = azurerm_key_vault.github_kv.id
-//
-//  lifecycle {
-//    ignore_changes = [value]
-//  }
-//}
+resource "azurerm_role_assignment" "deploy_principal_kv_role_assignment" {
+  scope                = module.key_vault.resource.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_key_vault_secret" "github_runners_access_token" {
+  name         = "github-runners-access-token"
+  value        = "placeholder"
+  key_vault_id = module.key_vault.resource.id
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+
+  depends_on = [azurerm_role_assignment.deploy_principal_kv_role_assignment]
+}
 
 
 
