770087c1bc6333e3181b679354d15bf8b4099f62
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-02-25 18:21:11 +0000

    Disable

--- management.tf
+++ management.tf
@@ -1,17 +1,18 @@
-//module "management_es" {
-//  source = "./modules/management-es"
-//
-//  count = var.skip_deploy ? 0 : (var.management_use_avm ? 0 : 1)
-//
-//  enable_telemetry = var.enable_telemetry
-//  settings         = local.management_settings_es
-//
-//  providers = {
-//    azurerm              = azurerm
-//    azurerm.connectivity = azurerm.connectivity
-//    azurerm.management   = azurerm.management
-//  }
-//}
+module "management_es" {
+  source = "./modules/management-es"
+
+  //count = var.skip_deploy ? 0 : (var.management_use_avm ? 0 : 1)
+  count = 0
+
+  enable_telemetry = var.enable_telemetry
+  settings         = local.management_settings_es
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm.connectivity
+    azurerm.management   = azurerm.management
+  }
+}
 
 module "management_avm" {
   source = "./modules/management-avm"
